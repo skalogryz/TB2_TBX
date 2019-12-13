7f9e6ab498747df8515d7ee63487e3dd41024207
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2019-12-13 09:22:49 -0500

    adding design package

---
+++ Packages/tb2_ide.pas
@@ -0,0 +1,22 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit tb2_ide;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  TB2RegLaz, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('TB2RegLaz', @TB2RegLaz.Register);
+end;
+
+initialization
+  RegisterPackage('tb2_ide', @Register);
+end.
